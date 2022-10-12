8223784def8902d5820bdc52ce249332ba983afe
EdgeDiary <1029033035@qq.com>
2022-10-12 19:08:26 +0800

    1

--- cakeShoppingApp/pages/index/index.vue
+++ cakeShoppingApp/pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
-			<text class="title">{{title}}</text>
+			<text class="title">为什么出错啊</text>
 		</view>
 	</view>
 </template>
